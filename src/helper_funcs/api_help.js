347477bd23a8f309fbeaab495f1abf906b90bd6d
JP-sDEV <jon.pablo80@gmail.com>
2020-11-02 11:52:04 -0500

    Updates

--- src/helper_funcs/api_help.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const get_year_avg = async (year) => {
-    const baseURL = "https://nba-clustering.herokuapp.com"
-    const res = await axios.get(`${baseURL}/${year}/avg/`)
-    return res.data
-}
